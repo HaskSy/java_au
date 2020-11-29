855c8df56c5b76c165b3382cd384880ebf7c91d2
hasksy <sergeykovaltsov@gmail.com>
2020-11-29 21:08:52 +0300

    Added 3DArray Iterator

---
+++ src/designpatterns/iterator/Main.java
@@ -0,0 +1,11 @@
+package designpatterns.iterator;
+
+public class Main {
+
+    Integer[][][] Array3d = {
+            {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {null, null, null, null}},
+            {{9, 9, 9, 9}, {9, 9, 9, 9}, {9, 9, 9, 9}, {null, null, null, 8}},
+            {{8,3,5,1}, {6,6,6,null}, {null, null, null, 12453}, {1,2,3,4}}
+    };
+
+}
